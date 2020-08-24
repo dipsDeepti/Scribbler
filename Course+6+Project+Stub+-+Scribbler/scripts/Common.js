58cc529b34e6f7f38e415ae01ea4c87ca11a2332
Deepti Sharma <deepti.sharma@ecolab.com>
2020-08-24 13:03:47 +0530

    first commit

---
+++ Course+6+Project+Stub+-+Scribbler/scripts/Common.js
@@ -0,0 +1,4 @@
+var headerContent = '<div class = "headerTitle"><span>ScriBBler</span> <span>Explore, Imagine, Create </span></div>' +
+'<div class = "headerContent"><button name="Sign Up" class="btn">Sign Up </button>'+
+'<button name="Sign In" class="btn">Sign In </button><div>';
+var header = document.getElementsByTagName('header')[0].innerHTML = headerContent;
